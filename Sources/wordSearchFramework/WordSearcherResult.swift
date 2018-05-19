6fad74bb5f691c6129a5afc539d8ef5411ca2234
Eric Downey <ericdowney@icloud.com>
2018-05-19 00:18:20 -0400

    Added implemented to add words with occurances to the result.

--- Sources/wordSearchFramework/WordSearcherResult.swift
+++ Sources/wordSearchFramework/WordSearcherResult.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 public struct WordSearcherResult {
+    typealias Position = (x: Int, y: Int)
+    
+    var words: [String: [Position]] = [:]
+    
+    /// Sets the positions for the occurances of words. Will override previously added words.
+    mutating func add(word: String, occuringAt positions: Position...) {
+        words[word] = positions
+    }
     
 }
