23cccbcb3c9ec04006189f9f87b9ef993474e11f
Eric Downey <ericdowney@icloud.com>
2018-05-19 01:28:14 -0400

    Added implementation to the WordSearchGrid. It checks the given words for min length requirements.

--- Sources/wordSearchFramework/WordSearchGrid.swift
+++ Sources/wordSearchFramework/WordSearchGrid.swift
@@ -9,6 +9,21 @@ import Foundation
 
 struct WordSearchGrid {
     
+    enum GridError: Error {
+        case minLengthViolation
+    }
     
+    // MARK: - Properties
     
+    var wordsToSearch: [String]
+    
+    // MARK: - Initializer
+    
+    init(words: [String]) throws {
+        let minCount = words.reduce(1000) { result, next in min(result, next.count) }
+        guard minCount >= 2 else {
+            throw GridError.minLengthViolation
+        }
+        self.wordsToSearch = words
+    }
 }
