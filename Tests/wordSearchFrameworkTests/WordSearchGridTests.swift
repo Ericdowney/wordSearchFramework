258afa02c387e4403e07e527c990d0fcf9631ef4
Eric Downey <ericdowney@icloud.com>
2018-05-19 01:22:44 -0400

    Wrote tests to verifiy initialization of the WordGrid. The grid should throw an error if one of the supplied words has a length less than 2. It should initialize properly when all of the words have a length greather than or equal to 2.

--- Tests/wordSearchFrameworkTests/WordSearchGridTests.swift
+++ Tests/wordSearchFrameworkTests/WordSearchGridTests.swift
@@ -5,21 +5,53 @@
 //  Created by Eric Downey on 5/19/18.
 //
 
-import Foundation
+import XCTest
 @testable import wordSearchFramework
 
-final class WordSearchGridTests {
+final class WordSearchGridTests: XCTestCase {
+    
+    var subject: WordSearchGrid!
+    
+    override func setUp() {
+        super.setUp()
+        
+        subject = try? WordSearchGrid(words: ["Hello", "There"])
+    }
+
     
     // MARK: - Tests
     
-    func testExample() {
+    func test_shouldThrowAnErrorWhenAGivenWordHasALengthLessThan2() {
+        let words = ["Hello", "There", "Goodbye", "1"]
         
+        do {
+            _ = try WordSearchGrid(words: words)
+            XCTFail()
+        }
+        catch {
+            XCTAssertEqual(WordSearchGrid.GridError.minLengthViolation, error as? WordSearchGrid.GridError)
+        }
+        
+    }
+    
+    func test_shouldInitializeProperlyWhenAllWordsHaveALengthGreaterOrEqualTo2() {
+        let words = ["Hello", "There", "Goodbye", "12"]
+        
+        do {
+            subject = try WordSearchGrid(words: words)
+            
+            XCTAssertEqual(subject.wordsToSearch, words)
+        }
+        catch {
+            XCTFail("The Word Grid should not throw an error")
+        }
     }
     
     // MARK: - Test Registration
     
     static var allTests = [
-        ("testExample", testExample),
+        ("test_shouldThrowAnErrorWhenAGivenWordHasALengthLessThan2", test_shouldThrowAnErrorWhenAGivenWordHasALengthLessThan2),
+        ("test_shouldInitializeProperlyWhenAllWordsHaveALengthGreaterOrEqualTo2", test_shouldInitializeProperlyWhenAllWordsHaveALengthGreaterOrEqualTo2)
     ]
     
 }
