c2b76d9daafb3983222e0e48e1e7c50f11df7fd5
Eric Downey <ericdowney@icloud.com>
2018-05-19 01:06:01 -0400

    Created two new files, WordSearchGrid and WordSearchGridTests. This new struct will be resposible for holding onto the word search grid and finding all word occurances.

---
+++ Sources/wordSearchFramework/WordSearchGrid.swift
@@ -0,0 +1,14 @@
+//
+//  WordSearchGrid.swift
+//  wordSearchFrameworkTests
+//
+//  Created by Eric Downey on 5/19/18.
+//
+
+import Foundation
+
+struct WordSearchGrid {
+    
+    
+    
+}
---
+++ Tests/wordSearchFrameworkTests/WordSearchGridTests.swift
@@ -0,0 +1,25 @@
+//
+//  WordSearchGridTests.swift
+//  wordSearchFrameworkTests
+//
+//  Created by Eric Downey on 5/19/18.
+//
+
+import Foundation
+@testable import wordSearchFramework
+
+final class WordSearchGridTests {
+    
+    // MARK: - Tests
+    
+    func testExample() {
+        
+    }
+    
+    // MARK: - Test Registration
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+    
+}
